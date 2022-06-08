8bf4ec920f3e66b5fd2c038f8e227f3a7382b432
Tushar Ahire <tusharmadhavahire98@gmail.com>
2022-06-08 21:14:31 +0530

    updated

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -3,22 +3,17 @@ import styles from "./addTask.module.css";
 
 const AddTask = ({func,tasks}) => {
   // NOTE: do not delete `data-testid` key value pair
-  let id;
-  if(tasks.length===0){
-    id=1;
-  }
-  else{
-    id=tasks[tasks.length-1].id+1
-  }
+  
+  
   const [text,setText]=useState({
-    "id":id ,
-    "text": "",
-    "done": false, 
-    "count": 1
+    id:null,
+    text:"",
+    done:false, 
+    count: 1
   })
 
 
- 
+ console.log(text.id)
 
 
   const uptext=(value)=>{
@@ -26,12 +21,19 @@ const AddTask = ({func,tasks}) => {
         ...text,
         text:value
       })
+      
+      
   }
 
   return (
     <div className={styles.todoForm}>
       <input onChange={(e)=>uptext(e.target.value)} data-testid="add-task-input" type="text" />
-      <button onClick={(()=>func([...tasks,text]))} data-testid="add-task-button">+</button>
+      <button onClick={(()=>{
+         setText({
+           ...text,
+           id:tasks.length+1
+         })
+        func([...tasks,text])})} data-testid="add-task-button">+</button>
     </div>
   );
 };
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -3,27 +3,29 @@ import styles from "./counter.module.css";
 
 const Counter = ({task,tasks,up}) => {
   // sample value to be replaced
-  let count = task.count;
+  
   // NOTE: do not delete `data-testid` key value pair
   const updatingC=(value)=>{
-    let ans=[...tasks]
-       for(var j=0;j<ans.length;j++){
-         if(ans[j].id===task.id){
-           if(ans[j].count>0){
+    
+       for(var j=0;j<tasks.length;j++){
+         if(tasks[j].id===task.id){
+           if(tasks[j].count>0){
+               let ans=[...tasks]
                 ans[j].count+=value
+                return up(ans)
            }
                
-                break;
+               
 
          }
        }
 
-       return up(ans)
+      
   }
   return (
     <div className={styles.counter}>
       <button onClick={()=>updatingC(1)} data-testid="task-counter-increment-button">+</button>
-      <span data-testid="task-counter-value">{count}</span>
+      <span data-testid="task-counter-value">{task.count}</span>
       <button onClick={()=>updatingC(-1)} data-testid="task-counter-decrement-button">-</button>
     </div>
   );
